59a5b4f73c6ebfa320378e913e505694987eaf29
Snehakhot <32100522+Snehakhot@users.noreply.github.com>
2017-09-19 19:25:38 +0530

    Add files via upload

---
+++ Analogreadtest.ino
@@ -0,0 +1,52 @@
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(9, 10);
+
+void setup() {
+  // put your setup code here, to run once:
+   mySerial.begin(9600);   // Setting the baud rate of GSM Module  
+  Serial.begin(9600);    // Setting the baud rate of Serial Monitor (Arduino)
+  delay(100);
+  int i=0;
+
+  
+}
+
+void loop() 
+{
+   // put your main code here, to run repeatedly:
+   int i;
+  if(i=0)
+  {
+      int sensorValue= analogRead(A0);
+    float voltage = sensorValue * (5.0 / 1023.0);
+    Serial.println(voltage);
+    if (voltage > 3.00)
+    {
+        Serial.println("Beyond Threshold Value");
+        //SendMessage();
+        i++;
+    }
+      else
+    {
+      Serial.println("Normal");
+    }
+  
+  }
+  else{ }
+ 
+}
+ void SendMessage()
+{
+  mySerial.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
+  delay(1000);  // Delay of 1000 milli seconds or 1 second
+  mySerial.println("AT+CMGS=\"+918412089776\"\r"); // Replace x with mobile number
+  delay(1000);
+  mySerial.println("Sensor value above threshold");// The SMS text you want to send
+  delay(100);
+   mySerial.println((char)26);// ASCII code of CTRL+Z
+  delay(1000);
+  
+}
+
